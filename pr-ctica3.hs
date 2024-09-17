d1e85d6ad89aba8f58252227e9031d3d50e5e0e0
Luisreyescc <luismanuelrc@ciencias.unam.mx>
2024-09-16 21:20:42 -0600

    Hecho el ejercicio 1 :)

--- pr-ctica3.hs
+++ pr-ctica3.hs
@@ -20,3 +20,19 @@ negar (Or p q) = And (negar p) (negar q)
 negar (And p q) = Or (negar p) (negar q)
 negar (Impl p q) = And p (negar q)
 negar (Syss p q) = Not (And (Impl p q) (Impl q p))
+
+-- Ejercicio q.
+fnn :: Prop -> Prop
+fnn (Var x) = Var x  
+fnn (Cons b) = Cons b 
+fnn (Not p) = case p of -- Usé esto, no se si esté permitido, lol
+    Not p' -> fnn p' -- Si p es (Not p) entonces fnn de p (pues doble negación)
+    _      -> Not (fnn p) -- En cualquier otro caso, hacemos fnn de p y aplicamos negación.
+fnn (And p q) = And (fnn p) (fnn q)  
+fnn (Or p q) = Or (fnn p) (fnn q)  
+fnn (Not (And p q)) = Or (fnn (Not p)) (fnn (Not q))  
+fnn (Not (Or p q)) = And (fnn (Not p)) (fnn (Not q)) 
+fnn (Impl p q) = fnn (Or (Not p) q)  
+fnn (Syss p q) = fnn (Or (And p q) (And (Not p) (Not q)))  
+
+

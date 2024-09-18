ea1193e20d1340a1da8d86c55758e9ac9ecda741
Luisreyescc <luismanuelrc@ciencias.unam.mx>
2024-09-17 18:01:31 -0600

    Ejercicios 1,2,3,4 hechos :)

--- pr-ctica3.hs
+++ pr-ctica3.hs
@@ -1,18 +1,18 @@
---Sintaxis de la logica proposicional
+--sintaxis de la logica proposicional
 data Prop = Var String | Cons Bool | Not Prop
             | And Prop Prop | Or Prop Prop
             | Impl Prop Prop | Syss Prop Prop
             deriving (Eq)
 
 instance Show Prop where 
-    show (Cons True) = "Verdadero"
-    show (Cons False) = "Falso"
-    show (Var p) = p
-    show (Not p) = "¬" ++ show p
-    show (Or p q) = "(" ++ show p ++ " ∨ " ++ show q ++ ")"
-    show (And p q) = "(" ++ show p ++ " ∧ " ++ show q ++ ")"
-    show (Impl p q) = "(" ++ show p ++ " → " ++ show q ++ ")"
-    show (Syss p q) = "(" ++ show p ++ " ↔ " ++ show q ++ ")"
+                    show (Cons True) = "Verdadero"
+                    show (Cons False) = "Falso"
+                    show (Var p) = p
+                    show (Not p) = "¬" ++ show p
+                    show (Or p q) = "(" ++ show p ++ " ∨ " ++ show q ++ ")"
+                    show (And p q) = "(" ++ show p ++ " ∧ " ++ show q ++ ")"
+                    show (Impl p q) = "(" ++ show p ++ " → " ++ show q ++ ")"
+                    show (Syss p q) = "(" ++ show p ++ " ↔ " ++ show q ++ ")"
 
 p, q, r, s, t, u :: Prop
 p = Var "p"
@@ -21,15 +21,33 @@ r = Var "r"
 s = Var "s"
 t = Var "t"
 u = Var "u"
-    
+
 negar :: Prop -> Prop
 negar (Var p) = Not (Var p)
-negar (Cons p) = Cons (not p)
-negar (Or p q) = And (negar p) (negar q)
-negar (And p q) = Or (negar p) (negar q)
-negar (Impl p q) = And p (negar q)
-negar (Syss p q) = Not (And (Impl p q) (Impl q p))
+negar (Cons True) = (Cons False)
+negar (Cons False) = (Cons True)
+negar (Not f) = f
+negar (And f1 f2) = (Or (negar f1) (negar f2))
+negar (Or f1 f2) = (And (negar f1) (negar f2))
+negar (Impl f1 f2) = (And f1 (negar f2))
+negar (Syss f1 f2)= negar (And (Impl f1 f2) (Impl f2 f1))
 
+-- Ejercicio 1.
+fnn :: Prop -> Prop
+fnn (Var x) = Var x  
+fnn (Cons b) = Cons b 
+fnn (Not p) = case p of -- Usé esto, no se si esté permitido lol. 
+    Not p' -> fnn p' -- Si p == "Not p" entonces aplicamos fnn a p solita (Doble negación).
+    And p q -> Or (fnn (Not p)) (fnn (Not q))  -- Si p es un And, entonces hacemos De Morgan para AND
+    Or p q -> And (fnn (Not p)) (fnn (Not q))  -- Si p es un Or, entonces hacemos De Morgan para OR
+    _      -> Not (fnn p)  -- En cualquier otro caso solo regresamos la negacion de fnn de p :)
+fnn (And p q) = And (fnn p) (fnn q)  
+fnn (Or p q) = Or (fnn p) (fnn q)  
+fnn (Impl p q) = fnn (Or (Not p) q)  
+fnn (Syss p q) = fnn (And (Impl p q) (Impl q p))
+
+-- Función auxiliar, distribuir aplica leyes de distribución.
+-- φ ∨ (ρ ∧ ψ) ≡ (φ ∨ ρ) ∧ (φ ∨ ψ) | φ ∧ (ρ ∨ ψ) ≡ (φ ∧ ρ) ∨ (φ ∧ ψ)
 distribuir :: Prop -> Prop
 distribuir (Or p (And q r)) = And (distribuir (Or p q)) (distribuir (Or p r))
 distribuir (Or (And q r) p) = And (distribuir (Or q p)) (distribuir (Or r p))
@@ -37,17 +55,16 @@ distribuir (Or p q) = Or (distribuir p) (distribuir q)
 distribuir (And p q) = And (distribuir p) (distribuir q)
 distribuir p = p
 
--- Ejercicio q.
-fnn :: Prop -> Prop
-fnn (Var x) = Var x  
-fnn (Cons b) = Cons b 
-fnn (Not p) = case p of -- Usé esto, no se si esté permitido, lol
-    Not p' -> fnn p' -- Si p es (Not p) entonces fnn de p (pues doble negación)
-    _      -> Not (fnn p) -- En cualquier otro caso, hacemos fnn de p y aplicamos negación.
-fnn (And p q) = And (fnn p) (fnn q)  
-fnn (Or p q) = Or (fnn p) (fnn q)  
-fnn (Not (And p q)) = Or (fnn (Not p)) (fnn (Not q))  
-fnn (Not (Or p q)) = And (fnn (Not p)) (fnn (Not q)) 
-fnn (Impl p q) = fnn (Or (Not p) q)  
-fnn (Syss p q) = fnn (Or (And p q) (And (Not p) (Not q))) 
+-- Ejercicio 2.
+fnc :: Prop -> Prop
+fnc p = fnn (distribuir p)
+
+-- Ejercicio 3.
+type Literal = Prop
+
+-- Ejercicio 4.
+type Clausula = [Literal]
+
+-- Ejercicio 5.
+-- clausulas :: Prop -> [Clausula]
 
